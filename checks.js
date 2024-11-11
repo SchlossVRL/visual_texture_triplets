b7bc7736f782d5d26d988d891204d194593788aa
Anna <annachinni@gmail.com>
2024-11-11 10:17:56 -0600

    upadting validation and checks

--- checks.js
+++ checks.js
@@ -1,36 +1,40 @@
-const xlsx = require('xlsx');
-const fs = require('fs');
+const xlsx = require("xlsx");
+const fs = require("fs");
 
 // Read the Excel file
-const workbook = xlsx.readFile('/Users/annachinni/Dropbox/TextureSemantics/VisualTextureTriplets/Triplet Code/Triplet Experiment/Triplet Data Cleaning and Setup/check_comparisons.xlsx');
+const workbook = xlsx.readFile(
+  "/Users/annachinni/Documents/GitHub/visual_texture_triplets/TripletDataCleaningSetup/check_comparisons_62.xlsx"
+);
 // Specify the sheet name you want to read
-const sheetName = 'Checks'; 
+const sheetName = "Checks";
 const sheet = workbook.Sheets[sheetName];
 
 // Convert the sheet to JSON
 const data = xlsx.utils.sheet_to_json(sheet);
 
 // Create the desired structure
-const result = data.map(row => ({
-    head: row.head,
-    choice_1: row.choice_1,
-    choice_2: row.choice_2,
-    correct_choice: row.correct_choice
+const result = data.map((row) => ({
+  head: row.head,
+  choice_1: row.choice_1,
+  choice_2: row.choice_2,
+  correct_choice: row.correct_choice,
 }));
 
 // Specify the name of the JSON file
-const jsonFileName = 'checks.json';
+const jsonFileName = "checks.json";
 
 // Custom stringify function to format the JSON as desired
 const customStringify = (obj) => {
-    return obj.map(item => {
-        return `{
+  return obj
+    .map((item) => {
+      return `{
     head: '${item.head}',
     choice_1: '${item.choice_1}',
     choice_2: '${item.choice_2}',
     correct_choice: '${item.correct_choice}'
   }`;
-    }).join(',\n');
+    })
+    .join(",\n");
 };
 
 // Save the result to a JSON file
--- validations.js
+++ validations.js
@@ -1,34 +1,38 @@
-const xlsx = require('xlsx');
-const fs = require('fs');
+const xlsx = require("xlsx");
+const fs = require("fs");
 
 // Read the Excel file
-const workbook = xlsx.readFile('/Users/annachinni/Dropbox/TextureSemantics/VisualTextureTriplets/Triplet Code/Triplet Experiment/Triplet Data Cleaning and Setup/validation_comparisons.xlsx');
+const workbook = xlsx.readFile(
+  "/Users/annachinni/Documents/GitHub/visual_texture_triplets/TripletDataCleaningSetup/validation_comparisons.xlsx"
+);
 // Specify the sheet name you want to read
-const sheetName = 'Randomized Sets'; 
+const sheetName = "Randomized Sets";
 const sheet = workbook.Sheets[sheetName];
 
 // Convert the sheet to JSON
 const data = xlsx.utils.sheet_to_json(sheet);
 
 // Create the desired structure
-const result = data.map(row => ({
-    head: row.head,
-    choice_1: row.choice_1,
-    choice_2: row.choice_2
+const result = data.map((row) => ({
+  head: row.head,
+  choice_1: row.choice_1,
+  choice_2: row.choice_2,
 }));
 
 // Specify the name of the JSON file
-const jsonFileName = 'validations.json';
+const jsonFileName = "validations.json";
 
 // Custom stringify function to format the JSON as desired
 const customStringify = (obj) => {
-    return obj.map(item => {
-        return `{
+  return obj
+    .map((item) => {
+      return `{
     head: '${item.head}',
     choice_1: '${item.choice_1}',
     choice_2: '${item.choice_2}'
   }`;
-    }).join(',\n');
+    })
+    .join(",\n");
 };
 
 // Save the result to a JSON file
