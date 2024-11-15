9f2bc5e6fce542cda9cc3bfbf48581e0b16a6255
Anna <annachinni@gmail.com>
2024-11-15 09:17:55 -0600

    get rid of extra stuff

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/annachinni/Documents/GitHub/visual_texture_triplets/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
